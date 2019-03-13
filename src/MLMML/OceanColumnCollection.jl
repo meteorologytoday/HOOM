d6406dd798253a39fc384197267f57c39b683390
xtt <meteorologytoday@gmail.com>
2019-03-12 21:29:51 -0700

    Start modifying OceanColumnCollection.jl

--- src/MLMML/OceanColumnCollection.jl
+++ src/MLMML/OceanColumnCollection.jl
@@ -1,21 +1,24 @@
 mutable struct OceanColumnCollection
-    N_ocs    :: Integer           # Number of columns
-
-    Nx    :: Integer           # Number of columns in i direction
-    Ny    :: Integer           # Number of columns in j direction
-
-    N        :: Integer           # Number of layers
+    Nx       :: Integer           # Number of columns in i direction
+    Ny       :: Integer           # Number of columns in j direction
+    Nz       :: Integer           # Number of layers
+    
     zs       :: Array{Float64, 1} # Position of (N+1) grid points
     K        :: Float64           # Diffusion coes
-    mask     :: Array{Float64}
+
+    mask     :: Array{Float64, 2}
     mask_idx :: Any
 
+    b_ML     :: Array{Float64, 2}
+    h_ML     :: Array{Float64, 2}
+
+    bs       :: Array{Float64, 3}
+    FLDO     :: Array{Float64, 2}
+
     # Derived quantities
+    N_ocs  :: Integer           # Number of columns
     hs     :: Array{Float64, 1} # Thickness of layers
     Δzs    :: Array{Float64, 1} # Δz between layers
-
-    
-    ocs    :: Array{MLMML.OceanColumn, 2}
 
     # workspace
     wksp   :: Workspace
@@ -23,7 +26,7 @@ mutable struct OceanColumnCollection
     function OceanColumnCollection(;
         Nx     :: Integer,
         Ny     :: Integer,
-        N      :: Integer,
+        Nz     :: Integer,
         zs     :: Array{Float64, 1},
         bs     :: Array{Float64, 1},
         K      :: Float64,
@@ -32,8 +35,6 @@ mutable struct OceanColumnCollection
         FLDO   :: Integer,
         mask   :: Union{Array{Float64, 2}, Nothing} = nothing,
     )
-
-        N_ocs = Nx * Ny
 
         hs  = zs[1:end-1] - zs[2:end]
         Δzs = (hs[1:end-1] + hs[2:end]) / 2.0
@@ -44,31 +45,50 @@ mutable struct OceanColumnCollection
         end
 
         mask_idx = (mask .== 0.0)
-        ocs = Array{MLMML.OceanColumn}(undef, Nx, Ny)
 
-        for i=1:Nx, j=1:Ny
+        h_ML = zeros(Float64, Nx, Ny)
+        b_ML = zeros(Float64, Nx, Ny)
 
-            if mask[i, j] == 0.0
-                continue
-            end
-
-            ocs[i, j] = MLMML.OceanColumn(
-                N = N,
-                zs = zs,
-                bs = bs,
-                K  = K,
-                b_ML = b_ML,
-                h_ML = h_ML,
-                FLDO = FLDO,
-                hs = hs,
-                Δzs = Δzs
-            )
-        end 
+        bs   = zeros(Float64, Nx, Ny, Nz)
+        FLDO = zeros(Float64, Nx, Ny)
 
         wksp = Workspace(Nx, Ny)
 
-        return new(N_ocs, Nx, Ny, N, zs, K, mask, mask_idx, hs, Δzs, ocs, wksp)
+        return new(
+            Nx, Ny, Nz,
+            zs, K,
+            mask, mask_idx,
+            b_ML, h_ML, bs, FLDO,
+            Nx * Ny, hs, Δzs,
+            ocs, wksp
+        )
     end
+
+
+    Nx       :: Integer           # Number of columns in i direction
+    Ny       :: Integer           # Number of columns in j direction
+    Nz       :: Integer           # Number of layers
+    
+    zs       :: Array{Float64, 1} # Position of (N+1) grid points
+    K        :: Float64           # Diffusion coes
+
+    mask     :: Array{Float64, 2}
+    mask_idx :: Any
+
+    b_ML     :: Array{Float64, 2}
+    h_ML     :: Array{Float64, 2}
+
+    bs       :: Array{Float64, 3}
+    FLDO     :: Array{Float64, 2}
+
+    # Derived quantities
+    N_ocs  :: Integer           # Number of columns
+    hs     :: Array{Float64, 1} # Thickness of layers
+    Δzs    :: Array{Float64, 1} # Δz between layers
+
+    # workspace
+    wksp   :: Workspace
+
 
 end
 
