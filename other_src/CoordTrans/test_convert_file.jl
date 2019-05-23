4c0b736aac396b2c1354e145fa1a740a30be454b
xtt <meteorologytoday@gmail.com>
2019-05-23 12:11:32 -0700

    Tranformation works

--- other_src/CoordTrans/test_convert_file.jl
+++ other_src/CoordTrans/test_convert_file.jl
@@ -4,8 +4,27 @@ using .WeightGeneration
 
 using Formatting
 
-in_filename  = "b.e11.B1850C5CN.f09_g16.005.pop.h.SHF.100001-109912.nc"
-out_filename = "test_SHF.nc"
+in_filename  = "b.e11.B1850C5CN.f09_g16.005.pop.h.SST.100001-109912.nc"
+out_filename = "test_SST.nc"
 wgt_filename = "wgt_gx1v6_to_gx3v7.nc"
 
-WeightGeneration.convertFile(in_filename, out_filename, wgt_filename, varnames2D=("SHF",))
+#=
+WeightGeneration.convertFile(
+    in_filename,
+    out_filename,
+    wgt_filename,
+    varnames=("SHF",);
+    xdim = "nlon",
+    ydim = "nlat",
+    zdim = "z_w",
+)
+=#
+WeightGeneration.convertFile(
+    in_filename,
+    out_filename,
+    wgt_filename,
+    varnames=("SST",);
+    xdim = "nlon",
+    ydim = "nlat",
+    zdim = "z_t",
+)
