8e6968b980163427276bca70dbe09e164ec178f5
xtt <meteorologytoday@gmail.com>
2019-05-25 15:46:55 -0700

    SST plot looks okay

--- other_src/diagnose_scripts/plot_SST.jl
+++ other_src/diagnose_scripts/plot_SST.jl
@@ -11,14 +11,14 @@ g   = 9.81      #  m / s^2
 Lw  = 2.26e6    #  J / kg
 
 casenames = [
-    "lowres_SSM_NK",
     "lowres_SSM_SOM",
+    "lowres_SSM_NK",
     "lowres_SSM_SOM_noQflux",
 ]
     
-linestyle = ["k-", "r-", "g-", "b-"]
+linestyle = ["r-", "g-", "b-"]
 
-Dataset("/glade/p/cesmdata/inputdata/share/domains/domain.lnd.fv4x5_gx3v7.091218.nc", "r") do ds
+Dataset("domain.lnd.fv4x5_gx3v7.091218.nc", "r") do ds
     global lat, lon, lev, ilev, rec
 
     lat = replace(ds["yc"][1,:], missing=>NaN)
@@ -37,9 +37,11 @@ for i = 1:length(casenames)
 
     casename = casenames[i]
 
-    Dataset("$casename/archive/ocn/$casename.ocn.h.ma.fv4x5.0001-0020.nc", "r") do ds
+    Dataset("$casename.ocn.h.ma.fv4x5.0001-0020.nc", "r") do ds
 
         t_rng = 240-5*12+1:240
+
+        println(ds.dim["time"])
 
         if match(r"SSM_NK", casename) != nothing
             T_ML = ds["T"][:, :, 1, t_rng]
@@ -50,7 +52,7 @@ for i = 1:length(casenames)
         end
     
         T_ML = replace(T_ML, missing=>NaN)
-        T_ML = nanmean(T_ML, dims=(1,3,))[1, :, 1]
+        T_ML = nanmean(T_ML, dims=(1,3,))[1, :, 1] .- 273.15
 
         ax[:plot](lat, T_ML, linestyle[i], linewidth=2,  label=casename)
         
@@ -62,5 +64,6 @@ ax[:legend]()
 ax[:set_xlabel]("Latitude [deg]")
 ax[:set_ylabel]("Sea Surface Temperature [degC]")
 ax[:set_xlim](-90, 90)
+ax[:set_ylim](-5, 30)
 
-fig[:savefig](format("compare_models_SST.png", casename), dpi=200)
+fig[:savefig]("compare_models_SST.png", dpi=200)
