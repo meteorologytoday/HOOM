aca5887cb2186b5800fa57e4e56f1bbabbb71ff4
xtt <meteorologytoday@gmail.com>
2019-06-07 17:08:13 -0700

    fix bug: output the 1st record t_cnt=1 which causes extra 1 record (day/month)

--- src/CESM_driver/cores/MLMML/CESMCORE_MLMML.jl
+++ src/CESM_driver/cores/MLMML/CESMCORE_MLMML.jl
@@ -155,49 +155,8 @@ module CESMCORE_MLMML
         write_restart :: Bool,
     )
 
-        if MD.configs["enable_short_term_archive"]
+        include(joinpath(@__DIR__, "..", "..", "common_record_code.jl"))
 
-            if MD.configs["daily_record"]
-                
-                if t_flags["new_month"] && substep == 1
-                        filename = format("{}.ocn.h.daily.{:04d}-{:02d}.nc", MD.casename, t[1], t[2])
-                        RecordTool.setNewNCFile!(
-                            MD.recorders["daily_record"],
-                            joinpath(MD.configs["short_term_archive_dir"], filename)
-                        )
-
-                        appendLine(MD.configs["short_term_archive_list"], filename)
-                end
-
-                RecordTool.record!(
-                    MD.recorders["daily_record"];
-                    avg_and_output = ( t_flags["new_day"] && substep==1 )
-                )
-
-            end
-            
-            if MD.configs["monthly_record"]
-
-                if t_flags["new_year"] && substep == 1
-                        filename = format("{}.ocn.h.monthly.{:04d}.nc", MD.casename, t[1])
-                        RecordTool.setNewNCFile!(
-                            MD.recorders["monthly_record"],
-                            joinpath(MD.configs["short_term_archive_dir"], filename)
-                        )
-
-                        appendLine(MD.configs["short_term_archive_list"], filename)
-                end
-
-                RecordTool.record!(
-                    MD.recorders["monthly_record"];
-                    avg_and_output = ( t_flags["new_month"] && substep==1 )
-                )
-
-
-            end
-
-        end
- 
         wksp = MD.wksp
 
         wksp.nswflx .*= -1.0
