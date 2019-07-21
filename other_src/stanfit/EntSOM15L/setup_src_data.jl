3e2c03fd053e02878ddc74a2a90dbd238d176cd4
Tien-Yiao Hsu <tienyiah@gplogin2.gp.local>
2019-07-21 15:43:34 -0700

    EntSOM

---
+++ other_src/stanfit/EntSOM15L/setup_src_data.jl
@@ -0,0 +1,15 @@
+using NCDatasets
+
+src_data_path = normpath(joinpath(dirname(@__FILE__)), "..", "..", "..", "data", "NCAR_LENS")
+
+F_filename = joinpath(src_data_path,   "transformed_b.e11.B1850C5CN.f45_g37.005.pop.h.SHF.100001-109912.nc")
+SST_filename = joinpath(src_data_path, "transformed_b.e11.B1850C5CN.f45_g37.005.pop.h.SST.100001-109912.nc")
+MLD_filename = joinpath(src_data_path, "transformed_b.e11.B1850C5CN.f45_g37.005.pop.h.HMXL.100001-109912.nc")
+
+Dataset(F_filename, "r") do ds
+    global nlon = ds.dim["nlon"]
+    global nlat = ds.dim["nlat"]
+end
+
+
+
---
+++ other_src/stanfit/EntSOM15L/submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --job-name=NKMLM-gamma
+#SBATCH --output=slurm.log
+#SBATCH --partition=nes2.8
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=2048
+#SBATCH --mail-user=tienyiah@uci.edu
+#
+#
+#SBATCH --array=1-100%100
+
+srun julia $SLURM_SUBMIT_DIR/single_x.jl --selected-index=$SLURM_ARRAY_TASK_ID
+
