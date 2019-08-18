c9ff828385bd48db54ecc5db37f121fabadf32cf
xtt <meteorologytoday@gmail.com>
2019-08-18 14:06:09 -0700

    put in example code

--- other_src/generate_cesm_case/example/makecases_cesm.sh
+++ other_src/generate_cesm_case/example/makecases_cesm.sh
@@ -3,8 +3,8 @@
 pwd_dir=$(pwd)
 
 echo "Current directory: $pwd_dir"
-compset=E_1850_CN
-machine=cheyenne
+compset=E_1850_CN_SPINUPOCN
+machine=xtt-centos-intel
 project_code=UCIR0029
 
 cases_dir=$pwd_dir/cases
@@ -41,48 +41,70 @@ model_settings=(
     NKOM  SOM_xQ        ""
 )
 
-#model_settings=(
-#    NKOM  SOM_oQ        "$raw_data_dir/docn_forcing.SOM.LENS.g37.nc"
-#)
+models=(
+    NKOM_SOM    "$raw_data_dir/docn_forcing.SOM.LENS.g37.nc"
+    NKOM_EntOM  "$raw_data_dir/docn_forcing.EntOM.LENS.g37.nc"
+    NKOM_FULL   ""
+)
 
+flow_schemes=(
+    oEK
+    xEK
+)
+
+
+relaxation_times=(
+    "0"
+    "20"
+    "100"
+)
 
 
 casenames=()
 
-for i in $(seq 1 $((${#model_settings[@]}/3))); do
-    model=${model_settings[$((3*(i-1)))]}
-    init_config=${model_settings[$((3*(i-1)+1))]}
-    qflux_file=${model_settings[$((3*(i-1)+2))]}
+for i in $(seq 1 $((${#models[@]}/2))); do
+    model=${models[$((2*(i-1)))]}
+    qflux_file=${models[$((2*(i-1)+1))]}
 
-    printf "Gonna making %s %s\n" $model $init_config
+    for j in $(seq 1 $((${#flow_schemes[@]}/1))); do
+        flow_scheme=${flow_schemes[$((1*(j-1)))]}
 
-    SMARTSLAB-main/other_src/generate_cesm_case/main.sh \
-        --code-output-dir=$code_output_dir              \
-        --init-files-dir=$init_files_dir                \
-        --label=$label                                  \
-        --resolution=$resolution                        \
-        --walltime="06:00:00"                           \
-        --data-clim-T-file=$T_file                      \
-        --data-clim-S-file=$S_file                      \
-        --topo-file=$topo_file                          \
-        --old-domain-file=$old_domain                   \
-        --new-domain-file=$new_domain                   \
-        --T-unit=C                                      \
-        --S-unit=PSU                                    \
-        --cesm-create-newcase=~/ucar_models/cesm1_2_2_1/scripts/create_newcase \
-        --compset=$compset                              \
-        --machine=$machine                              \
-        --model=$model                                  \
-        --init-config=$init_config                      \
-        --cesm-env=$cesm_env                            \
-        --ocn-ncpu=$ocn_ncpu                            \
-        --project-code=$project_code                    \
-        --qflux-file=$qflux_file                        \
-        --ocn-branch=$ocn_branch                        \
-        --single-job="$single_job"                      \
-        | tee tmp.txt
+        for k in $(seq 1 $((${#relaxation_times[@]}/1))); do
+            relaxation_time=${relaxation_times[$((1*(k-1)))]}
 
-        casenames+=($(tail -n 1 tmp.txt))
+
+            echo "##### MAKING: $model $flow_scheme $relaxation_time #####"
+
+            SMARTSLAB-main/other_src/generate_cesm_case/main.sh \
+                --code-output-dir=$code_output_dir              \
+                --init-files-dir=$init_files_dir                \
+                --label=$label                                  \
+                --resolution=$resolution                        \
+                --walltime="06:00:00"                           \
+                --data-clim-T-file=$T_file                      \
+                --data-clim-S-file=$S_file                      \
+                --topo-file=$topo_file                          \
+                --old-domain-file=$old_domain                   \
+                --new-domain-file=$new_domain                   \
+                --T-unit=C                                      \
+                --S-unit=PSU                                    \
+                --cesm-create-newcase=~/ucar_models/cesm1_2_2_1/scripts/create_newcase \
+                --compset=$compset                              \
+                --machine=$machine                              \
+                --model=$model                                  \
+                --flow-scheme=$flow_scheme                      \
+                --cesm-env=$cesm_env                            \
+                --ocn-ncpu=$ocn_ncpu                            \
+                --project-code=$project_code                    \
+                --qflux-file=$qflux_file                        \
+                --ocn-branch=$ocn_branch                        \
+                --single-job="$single_job"                      \
+                --relaxation-time=$relaxation_time                      \
+                | tee tmp.txt
+
+                casenames+=($(tail -n 1 tmp.txt))
+        done
+    done
 done
 
 all_cmd="$code_output_dir/00_all_cmd.sh"
