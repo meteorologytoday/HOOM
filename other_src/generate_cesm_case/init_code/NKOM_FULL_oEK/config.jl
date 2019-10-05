d55e0a4f6c8353d1f7a61b9c4e5a63ed68bcb7f7
xtt <meteorologytoday@gmail.com>
2019-10-05 13:29:25 -0700

    Update qflux scheme keywords

--- other_src/generate_cesm_case/init_code/NKOM_FULL_oEK/config.jl
+++ other_src/generate_cesm_case/init_code/NKOM_FULL_oEK/config.jl
@@ -1,6 +1,6 @@
 merge!(overwrite_configs, Dict(
     :MLD_scheme                   => :prognostic,
-    :Qflux_scheme                 => :off,
+    :Qflux_scheme                 => :energy_flux,
     :vertical_diffusion_scheme    => :off,
     :horizontal_diffusion_scheme  => :off,
     :relaxation_scheme            => :on,
