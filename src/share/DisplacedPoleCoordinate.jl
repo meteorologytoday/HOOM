a8653779c998a9dda42e56931dffe28756b940fc
xtt <meteorologytoday@gmail.com>
2019-10-30 22:48:19 -0700

    fix bug

--- src/share/DisplacedPoleCoordinate.jl
+++ src/share/DisplacedPoleCoordinate.jl
@@ -103,7 +103,7 @@ struct GridInfo
         c_lat  :: AbstractArray{Float64, 2},  # center latitude
         vs_lon :: AbstractArray{Float64, 3},  # vertices longitude (4, Nx, Ny)
         vs_lat :: AbstractArray{Float64, 3},  # vertices latitude  (4, Nx, Ny)
-        area   :: AbstractArray{Float64, 3};  # area in radian^2
+        area   :: AbstractArray{Float64, 2};  # area in radian^2
         angle_unit :: Symbol = :deg,
         sub_yrng :: Union{Colon, UnitRange} = Colon(),
     )
