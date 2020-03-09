ddcfe30aaf6b295066ca72b5d6885eae4e41b9b3
Tien-Yiao Hsu <tienyiao@cheyenne2.cheyenne.ucar.edu>
2020-03-08 19:59:07 -0600

    cal SAS

--- other_src/Qflux_adjustment/mk_correction_file_monthly.jl
+++ other_src/Qflux_adjustment/mk_correction_file_monthly.jl
@@ -58,6 +58,10 @@ let
     global Qflx_T_correction = zeros(Float64, ni, nj, 12)
     global Qflx_S_correction = zeros(Float64, ni, nj, 12)
 
+    global TSAS = zeros(Float64, ni, nj)
+    global SSAS = zeros(Float64, ni, nj)
+
+
     for m=1:12
 
         println(format("Doing month : {:02d}", m))
@@ -66,6 +70,8 @@ let
             Dataset(format("{:s}{:04d}-{:02d}.nc", parsed["data-file-prefix"], y, m)) do ds
                 Qflx_T_correction[:, :, m] +=  nomissing( ds["qflx_T_correction"][:, :, 1] )
                 Qflx_S_correction[:, :, m] +=  nomissing( ds["qflx_S_correction"][:, :, 1] )
+                TSAS +=  nomissing( ds["TSAS_clim"][:, :, 1] )
+                SSAS +=  nomissing( ds["SSAS_clim"][:, :, 1] )
             end
         end
     end
@@ -73,6 +79,10 @@ let
     nyears = parsed["end-year"] - parsed["beg-year"] + 1 
     Qflx_T_correction /= nyears
     Qflx_S_correction /= nyears
+    TSAS /= nyears * 12
+    SSAS /= nyears * 12
+
+    TSAS *= 3996.0 * 1026.0
 
     # Apply mask
     idx = mask .== 0.0
@@ -81,15 +91,24 @@ let
         view(Qflx_S_correction, :, :, m)[idx] .= NaN
     end
 
+    println("Size of TSAS", size(TSAS))
+
+    TSAS[idx] .= NaN
+    SSAS[idx] .= NaN
+
     idx = mask .== 1.0
     _area = area[idx]
     _area_sum = sum(_area)
     global avg_Qflx_T_correction = sum(_area .* (mean(Qflx_T_correction, dims=3)[:, :, 1])[idx]) / _area_sum
     global avg_Qflx_S_correction = sum(_area .* (mean(Qflx_S_correction, dims=3)[:, :, 1])[idx]) / _area_sum
+    global avg_TSAS = sum(_area .* TSAS[idx]) / _area_sum
+    global avg_SSAS = sum(_area .* SSAS[idx]) / _area_sum
 end
 
 println("avg_Qflx_T_correction: ", avg_Qflx_T_correction )
 println("avg_Qflx_S_correction: ", avg_Qflx_S_correction )
+println("avg_TSAS: ", avg_TSAS )
+println("avg_SSAS: ", avg_SSAS )
 
 
 
@@ -101,6 +120,8 @@ Dataset(parsed["output-file"], "c") do ds
 
     ds.attrib["avg_Qflx_T_correction"] = avg_Qflx_T_correction
     ds.attrib["avg_Qflx_S_correction"] = avg_Qflx_S_correction
+    ds.attrib["avg_TSAS"] = avg_TSAS
+    ds.attrib["avg_SSAS"] = avg_SSAS
 
     for (varname, vardata, vardim, attrib) in [
         ("Qflx_T_correction", Qflx_T_correction, ("ni", "nj", "time"), Dict()),
