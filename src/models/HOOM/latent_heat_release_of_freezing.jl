73f737d3119d12c3de7101d843eda8c887ab9e2a
xtt <meteorologytoday@gmail.com>
2020-03-11 23:32:58 -0700

    Fix bug: since density is nonlinear now, we need to do advective adjustment after generating ice

--- src/models/HOOM/latent_heat_release_of_freezing.jl
+++ src/models/HOOM/latent_heat_release_of_freezing.jl
@@ -31,13 +31,12 @@ function calLatentHeatReleaseOfFreezing!(ocn::Ocean; Δt::Float64)
             OC_updateB!(ocn, i, j)
         end
 
-        #=
-        old_FLDO=ocn.FLDO[i,j]
-        Δb = (old_FLDO == -1 ) ? 0.0 : ocn.b_ML[i, j] - ocn.bs[old_FLDO, i, j]
-
-        if Δb < -3e-6
-            println(format("[Qflx2ATM] [AFTER] At {:d}, {:d}: Δb = {:f}. After adjustment.", i, j, Δb))
-        end
-        =#
     end
+
+    if do_convadjust
+        @loop_hor ocn i j let
+            OC_doConvectiveAdjustment!(ocn, i, j;)
+        end
+    end
+
 end
