10d182af77cc3c568ec096167ec151ec4063898b
Tien-Yiao Hsu <tienyiao@cheyenne2.cheyenne.ucar.edu>
2020-06-01 13:23:19 -0600

    Diagnose ice in both hemisphere

--- other_src/diagnose_scripts/diagnose_single_model.sh
+++ other_src/diagnose_scripts/diagnose_single_model.sh
@@ -186,7 +186,7 @@ if [ -f flag_diag_fornow ] ; then
     julia $script_analysis_dir/atm_ice.jl --data-file-prefix="$atm_hist_dir/$casename.cam.h0." --data-file-timestamp-form=YEAR_MONTH --domain-file=$atm_domain --output-file=$ice_analysis1 --beg-year=$diag_beg_year --end-year=$diag_end_year
 
     # Global Average Temperature analysis
-    julia $script_analysis_dir/atm_temperature.jl --data-file-prefix="$atm_hist_dir/$casename.cam.h0." --data-file-timestamp-form=YEAR_MONTH --domain-file=$atm_domain --output-file=$atm_analysis7 --beg-year=$diag_beg_year --end-year=$diag_end_year
+    #julia $script_analysis_dir/atm_temperature.jl --data-file-prefix="$atm_hist_dir/$casename.cam.h0." --data-file-timestamp-form=YEAR_MONTH --domain-file=$atm_domain --output-file=$atm_analysis7 --beg-year=$diag_beg_year --end-year=$diag_end_year
  
 fi
 
