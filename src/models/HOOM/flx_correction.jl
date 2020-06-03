ead3c63b341d99c9dc2bd39f815c38bc41c93010
Tien-Yiao Hsu <tienyiao@cheyenne2.cheyenne.ucar.edu>
2020-06-03 13:47:52 -0600

    change the nudging formula

--- src/models/HOOM/flx_correction.jl
+++ src/models/HOOM/flx_correction.jl
@@ -30,19 +30,30 @@ function calFlxCorrection!(
         # Euler backward method
 
         ifrac = ocn.in_flds.ifrac[i, j]
+        ifrac_clim = ocn.in_flds.IFRACclim[i, j]
         T_ML = ocn.T_ML[i, j]
         S_ML = ocn.S_ML[i, j]
         FLDO = ocn.FLDO[i, j]
         h_ML = ocn.h_ML[i, j]
  
-        ΔT = rr * (ocn.in_flds.Tclim[i, j] - T_ML)
+
         ΔS = rr * (ocn.in_flds.Sclim[i, j] - S_ML)
 
-        # Assume seaice thickenss = 1.0m
-        energy_to_melt_seaice = 1.0 * (ifrac - ocn.in_flds.IFRACclim[i, j]) * ρ_si * Hf_sw
-        ΔT_to_melt_seaice = energy_to_melt_seaice / h_ML / ρc_sw * r
+        # when ifrac == 0 then SST is not changing while energy are still flowing
+        # the solution is to use ifrac as additional information to recover Qflx_T
+        ΔT_openocn = rr * (ocn.in_flds.Tclim[i, j] - T_ML)
 
-        ΔT += ΔT_to_melt_seaice
+        # Assume seaice thickenss = 1.0m
+        energy_to_melt_seaice = 1.0 * (ifrac - ifrac_clim) * ρ_si * Hf_sw
+        ΔT_seaice = energy_to_melt_seaice / h_ML / ρc_sw * r
+        #=
+        0.00 => 0
+        0.05 => 1
+        let ΔT_seaice takeover completely the temperature nudging if ifrac differs by more than 5% 
+        =#
+        wgt_seaice= max( min( 0 + (1 - 0) / (0.05 - 0.00) * abs(ifrac - ifrac_clim), 1.0), 0.0)
+        ΔT = ΔT_openocn * (1.0 - wgt_sea_ice) + ΔT_seaice * wgt_sea_ice 
+
 
         T_ML += ΔT
         S_ML += ΔS
